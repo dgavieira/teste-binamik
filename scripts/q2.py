e489ada767114ce159aae4bca75fa2e42e3a2a74
Diego Vieira <dgavieira@users.noreply.github.com>
2021-02-20 15:30:32 -0400

    Sum N
    estruturação dos dados

---
+++ scripts/q2.py
@@ -0,0 +1,24 @@
+"""
+    Sum N
+    Given an A array and a N number, return True if there are numbers X and Y in the A array such that
+    X + Y = N. Otherwise, return False.
+    Example:
+        Input: [1,2,4,7]
+        Output: True (2 + 7 = 9)
+        Input: [3, 4, 6], 6
+        Output: False (3 + 4 = 7, 3 + 6 = 9, 4 + 6 = 10)
+"""
+import numpy as np
+
+def sum_n(A, N):
+    if case == POSITIVE:
+
+        return True
+    else:
+        return False
+
+
+
+if __name__ == '__main__':
+
+    sum_n(A, N)
