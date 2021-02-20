4e0b59ac901e82a4dc2d748eb791ed3b41454b2b
Diego Vieira <dgavieira@users.noreply.github.com>
2021-02-20 15:03:01 -0400

    Docstrings no união array
    Documentação do primeiro código

--- scripts/q1.py
+++ scripts/q1.py
@@ -1,17 +1,51 @@
 import numpy as np
 
-A = [5,7,8,10,15]
-B = [2,5,7,9,12]
+'''    
+    Union of two arrays
+    Write a function which returns the union of two sorted arrays. The resulting array should stay sorted.
+    Example:
+    Input: Array A [5, 7, 8, 10, 15], Array B [2, 5, 7, 9, 12]
+    Output: Array C [2, 5, 7, 8, 9, 10, 12, 15]
+'''
 
-arrA = np.array(A)
-arrB = np.array(B)
 
-arrC = np.concatenate([arrA, arrB])
+def uniao_arrays(A, B):
+    """ Parameters
+        ----------
+        A: list
+            List of numbers that could be sorted or not. Duplicates allowed.
+        B: list
+            List of numbers that could be sorted or not. Duplicates allowed.
 
-sortedC = np.sort(arrC)
+        Returns
+        --------
+        C: list
+            Sorted and concatenated list which has no duplicates.
+    """
 
-sortedC = np.unique(sortedC)
+    # convert type from list or tuple to numpy array
+    arrA = np.array(A)
+    arrB = np.array(B)
 
-C = sortedC.tolist()
+    # grants sorted arrays
+    arrA = np.sort(arrA)
+    arrB = np.sort(arrB)
 
-print(C)
+    # concatenate arrays A and B into a union array C
+    arrC = np.concatenate([arrA, arrB])
+
+    # sorts array C
+    sortedC = np.sort(arrC)
+    
+    # remove duplicates
+    sortedC = np.unique(sortedC)
+
+    # changes type to list and returns
+    C = sortedC.tolist()
+    return C
+
+
+if __name__ == '__main__':
+    A = [5, 7, 8, 10, 15]
+    B = [2, 5, 7, 9, 12]
+    uniao_arrays(A, B)
