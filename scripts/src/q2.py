da5b53e2e74ce86ce59ad1953062b709e4ba81d6
Diego Vieira <dgavieira@users.noreply.github.com>
2021-02-22 09:33:26 -0400

    Casos positivos e falsos para q2.py

--- scripts/src/q2.py
+++ scripts/src/q2.py
@@ -3,22 +3,28 @@
     Given an A array and a N number, return True if there are numbers X and Y in the A array such that
     X + Y = N. Otherwise, return False.
     Example:
-        Input: [1,2,4,7]
+        Input: [1,2,4,7], 9
         Output: True (2 + 7 = 9)
         Input: [3, 4, 6], 6
         Output: False (3 + 4 = 7, 3 + 6 = 9, 4 + 6 = 10)
 """
-import numpy as np
+
 
 def sum_n(A, N):
-    if case == POSITIVE:
-
-        return True
-    else:
-        return False
-
+    for i in range(len(A)):
+        for j in range(i + 1, len(A)):
+            if A[i] + A[j] == N:
+                return True
+    return False
 
 
 if __name__ == '__main__':
+    A = [1, 2, 4, 7]
+    N = 9
+    C = sum_n(A, N)
+    print(C)
 
-    sum_n(A, N)
+    A = [3,4,6]
+    N = 6
+    C = sum_n(A,N)
+    print(C)
